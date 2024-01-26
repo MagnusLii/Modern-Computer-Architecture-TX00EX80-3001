05063341f49ab27ac26f03a164dda8a744266699
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-01-26 14:53:29 +0200

    reset ex4

--- Oma-Tasks/04-Conditional-branch/exercise4.c
+++ Oma-Tasks/04-Conditional-branch/exercise4.c
@@ -10,11 +10,9 @@ __attribute__(( naked )) int difference(int a, int b)
     asm volatile
     (
         "push {r4, r5, r6, r7} \n" // do not remove
-        "mov r0 \n"
-        "mov r1 \n"
-        "sub r0, r0, r1 \n"
-        "mov r2, #0x80000000 \n" // create a mask with the sign bit set to 1
-        "eor r0, r0, r2 \n" // XOR r0 and r2
+        // execute compare instruction to set flags before conditional branch
+        // "cmp ..... \n"
+        // some code here
         "pop {r4, r5, r6, r7} \n" // do not remove
         "bx lr \n" // do not remove
     );
